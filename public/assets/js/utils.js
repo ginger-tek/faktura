702dfa4b58b65468746e4abaa434a9ecc3441777
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-06-20 18:33:08 -0400

    rename/add utils

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -13,11 +13,23 @@ export const api = async (u, m = null, b = null, t = null) => {
   return res.ok ? data : Promise.reject({ ...data, status: res.status })
 }
 
-export const money = (amount = 0) => {
+export const toMoney = (amount = 0) => {
   return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+}
+
+export const toDate = (timestamp, format = 'datetime') => {
+  let dtObj
+  if (typeof timestamp === 'string')
+    dtObj = new Date(timestamp)
+  else if (typeof timestamp === 'number')
+    dtObj = new Date(timestamp * 1000)
+  if (format === 'date') return dtObj.toLocaleDateString()
+  else if (format === 'time') return dtObj.toLocaleTimeString()
+  return dtObj.toLocaleString()
 }
 
 export default {
   api,
-  money
+  toMoney,
+  toDate
 }
