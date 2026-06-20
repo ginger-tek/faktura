641695e5f39b8a61bd2c6101ddf246d7af7a1074
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-06-20 18:50:53 -0400

    setup org default data on create, change args to array

--- src/Services/OrgsDataService.php
+++ src/Services/OrgsDataService.php
@@ -7,11 +7,36 @@ use Faktura\Data\Db;
 
 class OrgsDataService
 {
-  public static function create(string $name, string $code): object
+  public static function create(array $data): object
   {
     $id = Uuid::uuid4()->toString();
-    Db::run("insert into orgs (id, name, org_code)
-    values (?, ?, ?)", [$id, $name, $code]);
+    Db::run("insert into orgs (id, name, org_code, created_by)
+    values (?, ?, ?, ?)", [
+      $id,
+      $data['name'],
+      $data['org_code'],
+      $data['user_id']
+    ]);
+    Db::run("insert into org_settings (org_id, setting_key, setting_value, created_by)
+    values (:org_id, 'default_labor_rate', '60.00', :created_by),
+    (:org_id, 'invoice_template', '{{ invoice.items }}', :created_by),
+    (:org_id, 'contact_website', '', :created_by),
+    (:org_id, 'contact_email', '', :created_by),
+    (:org_id, 'contact_phone', '', :created_by),
+    (:org_id, 'contact_address', '', :created_by)", [
+      ':org_id' => $id,
+      ':created_by' => $data['user_id']
+    ]);
+    Db::run("insert into roles (org_id, role_name, bit_value, created_by)
+    values (:org_id, 'admin', :admin_bit_value, :created_by),
+    (:org_id, 'reader', :reader_bit_value, :created_by),
+    (:org_id, 'user', :user_bit_value, :created_by)", [
+      ':org_id' => $id,
+      ':admin_bit_value' => PermissionsService::getAllPermissionsBitValue(),
+      ':reader_bit_value' => PermissionsService::getPermissionsByNameFilter('INVOICE_READ|CLIENT_READ|EXPENSE_READ', true),
+      ':user_bit_value' => PermissionsService::getPermissionsByNameFilter('INVOICE_|CLIENT_|EXPENSE_', true),
+      ':created_by' => $data['user_id']
+    ]);
     return self::getById($id);
   }
 
