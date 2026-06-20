3664505c689f991f1d4bdb574eaf5c6f6e9ed32d
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-06-20 18:33:26 -0400

    new view

---
+++ public/assets/js/views/dashboard.js
@@ -0,0 +1,11 @@
+export default {
+  template: `<div>
+    <article>
+      <header><b>Calendar</b></header>
+      <p>Calendar view of your invoices and expenses coming soon!</p>
+    </article>
+  </div>`,
+  setup() {
+    // Future implementation for dashboard features can be added here
+  }
+}
