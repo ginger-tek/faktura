96f2f8d05aa4cd6baa4e3f8cec70a9cfe0d2670b
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-06-23 23:33:56 -0400

    add fields on create, set created_at/updated_at manually using time() instead of executing SQL unixepoch() func for default values on insert/update, use new roles view

--- src/Services/RolesDataService.php
+++ src/Services/RolesDataService.php
@@ -10,31 +10,33 @@ class RolesDataService
   public static function create(array $data): object
   {
     $id = Uuid::uuid4()->toString();
-    Db::run("insert into roles (id, org_id, role_name, created_by)
-    values (?, ?, ?, ?)", [
+    Db::run("insert into roles (id, org_id, role_name, bit_value, created_by, created_at)
+    values (?, ?, ?, ?, ?, ?)", [
       $id,
       $data['org_id'],
       $data['role_name'],
-      $data['created_by']
+      $data['bit_value'] ?? 0,
+      $data['created_by'] ?? null,
+      time()
     ]);
     return self::getById($id, $data['org_id']);
   }
 
   public static function getById(string $id, string $org_id): ?object
   {
-    return Db::run("select * from roles
+    return Db::run("select * from v_roles
     where id = ? and org_id = ?", [$id, $org_id])->fetch() ?: null;
   }
 
   public static function getByName(string $org_id, string $role_name): ?object
   {
-    return Db::run("select id from roles
+    return Db::run("select * from v_roles
     where org_id = ? and role_name = ?", [$org_id, $role_name])->fetch() ?: null;
   }
 
   public static function list(string $org_id): array
   {
-    return Db::run("select * from roles
+    return Db::run("select * from v_roles
     where org_id = ?", [$org_id])->fetchAll();
   }
 
@@ -44,11 +46,12 @@ class RolesDataService
       role_name = ?,
       bit_value = ?,
       updated_by = ?,
-      updated_at = (unixepoch())
+      updated_at = ?
     where id = ? and org_id = ?", [
       $role->role_name,
       $role->bit_value,
       $role->updated_by,
+      time(),
       $role->id,
       $role->org_id
     ]);
