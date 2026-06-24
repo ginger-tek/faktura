a01688332ee087c200b03973d5b01fea915c8981
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-06-23 23:24:56 -0400

    add optional chaining

--- public/assets/js/comps/created-updated.js
+++ public/assets/js/comps/created-updated.js
@@ -5,12 +5,12 @@ export default {
     obj: { type: Object, required: true }
   },
   template: `<small>
-    <div v-if="obj.created_by"><i class="bi bi-plus-circle"></i> {{ created }}</div>
-    <div v-if="obj.updated_by"><i class="bi bi-clock-history"></i> {{ updated }}</div>
+    <div v-if="obj?.created_by"><i class="bi bi-plus-circle"></i> {{ created }}</div>
+    <div v-if="obj?.updated_by"><i class="bi bi-clock-history"></i> {{ updated }}</div>
   </small>`,
   setup(props) {
-    const created = Vue.computed(() => props.obj.created_by ? `${props.obj.created_by?.split('|')[0]} @ ${toDate(props.obj.created_at)}` : '')
-    const updated = Vue.computed(() => props.obj.updated_by ? `${props.obj.updated_by?.split('|')[0]} @ ${toDate(props.obj.updated_at)}` : '')
+    const created = Vue.computed(() => props.obj?.created_by ? `${props.obj.created_by?.split('|')[0]} @ ${toDate(props.obj.created_at)}` : '')
+    const updated = Vue.computed(() => props.obj?.updated_by ? `${props.obj.updated_by?.split('|')[0]} @ ${toDate(props.obj.updated_at)}` : '')
 
     return { created, updated }
   }
