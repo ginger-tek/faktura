5bbea75d770ff5b05311e6357c3a5146f7005c71
ginger-tek <moormann.jeremy.tech@gmail.com>
2026-07-04 15:06:05 -0400

    added prefixes

--- public/assets/js/comps/created-updated.js
+++ public/assets/js/comps/created-updated.js
@@ -5,8 +5,8 @@ export default {
     obj: { type: Object, required: true }
   },
   template: `<small>
-    <div v-if="obj?.created_by"><i class="bi bi-plus-circle"></i> {{ created }}</div>
-    <div v-if="obj?.updated_by"><i class="bi bi-clock-history"></i> {{ updated }}</div>
+    <div v-if="obj?.created_by"><i class="bi bi-plus-circle"></i> Created by {{ created }}</div>
+    <div v-if="obj?.updated_by"><i class="bi bi-clock-history"></i> Updated by {{ updated }}</div>
   </small>`,
   setup(props) {
     const created = Vue.computed(() => props.obj?.created_by ? `${props.obj.created_by?.split('|')[0]} @ ${toDate(props.obj.created_at)}` : '')
